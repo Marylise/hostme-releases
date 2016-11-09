38ff9536677db11adf0ed3d5e87ede5dd90f6563
Evgeny Popov <evgeny.popov@hostmeapp.com>
2016-11-08 17:28:40 -0800

    ddd

--- Store/postbuild.sh
+++ Store/postbuild.sh
@@ -2,4 +2,6 @@ echo ">> Post-build script. Copy files from [$2] to [$3]"
 
 find $1 -type f -name '*.txt' -print -exec cp \{\} $2 \;
 
+find $2 -name '*.txt' -exec echo "File is {}" \;
+
 #cp $1 $2 $3
